7c341876b6b432eaa8b47eee5284cb65864b585f
Poliana Dias <polianadias@outlook.com>
2016-05-19 13:57:59 -0300

    Update Produto.cs

--- src/ArquiteturaModelo.Dominio/Entidades/Produto.cs
+++ src/ArquiteturaModelo.Dominio/Entidades/Produto.cs
@@ -4,5 +4,16 @@
     {
         public int ProdutoId { get; set; }
         public string Descricao { get; set; }
+        
+        public ValidationResult ValidationResult { get; private set; }
+
+        public bool IsValid
+        {
+            get
+            {
+                ValidationResult = new ProdutoEstaConsistenteValidation().Valid(this);
+                return ValidationResult.IsValid;
+            }
+        }
     }
 }
