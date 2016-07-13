215b58cf2a7197929f5e232d4051e7c488ffbb16
Poliana Dias <polianadias@outlook.com>
2016-07-12 23:48:49 -0300

    Removendo XML do retorno da API
    Removendo o tipo de retorno XML da API e deixando apenas JSON.

--- src/ArquiteturaModelo.Servicos.REST.ModeloAPI/App_Start/WebApiConfig.cs
+++ src/ArquiteturaModelo.Servicos.REST.ModeloAPI/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace ArquiteturaModelo.Servicos.REST.ModeloAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+              var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
