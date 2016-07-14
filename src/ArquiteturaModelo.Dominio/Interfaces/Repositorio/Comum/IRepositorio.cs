6ffc159cd56be55fedcf8cb4c2a5a272938bd1dc
Poliana Dias <polianadias@outlook.com>
2016-07-14 16:53:07 -0300

    Atualização no repositório genérico
    Inclusão do método obterPor para selecionar dados com parâmetros diferentes.

--- src/ArquiteturaModelo.Dominio/Interfaces/Repositorio/Comum/IRepositorio.cs
+++ src/ArquiteturaModelo.Dominio/Interfaces/Repositorio/Comum/IRepositorio.cs
@@ -5,11 +5,12 @@ namespace ArquiteturaModelo.Dominio.Interfaces.Repositorio.Comum
 {
     public interface IRepositorio<TEntity> where TEntity : class
     {
-        void Adicionar(TEntity entity, IDbTransaction transaction = null, int? commandTimeout = null);
-        void Atualizar(TEntity entity, IDbTransaction transaction = null, int? commandTimeout = null);
-        void Deletar(TEntity entity, IDbTransaction transaction = null, int? commandTimeout = null);
-        TEntity ObterPorId(int id);
-        IEnumerable<TEntity> ObterTodos();
+        dynamic Adicionar(TEntity entity, IDbTransaction transaction = null, int? commandTimeout = null);
+        bool Atualizar(TEntity entity, IDbTransaction transaction = null, int? commandTimeout = null);
+        bool Deletar(TEntity entity, IDbTransaction transaction = null, int? commandTimeout = null);
+        TEntity ObterPorId(int id, IDbTransaction transaction = null, int? commandTimeout = null);
+        IEnumerable<TEntity> ObterTodos(IDbTransaction transaction = null, int? commandTimeout = null);
+        IEnumerable<TEntity> ObterPor(object where = null, object order = null, IDbTransaction transaction = null, int? commandTimeout = null);
         
     }
 }
